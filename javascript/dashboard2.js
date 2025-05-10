d66ed1ceba3bbe7f5fb82ddb28e2d1f9b240a66e
Thiago.GT.NEV <caselasthiago@gmail.com>
2025-05-09 23:12:48 -0300

    ideia-1

--- javascript/dashboard2.js
+++ javascript/dashboard2.js
@@ -1,14 +1,12 @@
-const percent = 50; // Mude isso futuramente com o score da pessoa
-const circle = document.querySelector('.progress-ring__circle');
-const text = document.getElementById('score-text');
+  const percent = 65; // muda esse valor pra o score real
+  const circle = document.querySelector('.progress-ring__circle');
+  const text = document.getElementById('score-text');
 
-const radius = circle.r.baseVal.value;
-const circumference = 2 * Math.PI * radius;
+  const radius = 65;
+  const circumference = 2 * Math.PI * radius;
 
-circle.style.strokeDasharray = `${circumference}`;
-circle.style.strokeDashoffset = circumference;
+  circle.style.strokeDasharray = `${circumference}`;
+  const offset = circumference - (percent / 100) * circumference;
+  circle.style.strokeDashoffset = offset;
 
-const offset = circumference - (percent / 100) * circumference;
-circle.style.strokeDashoffset = offset;
-
-text.textContent = `${percent}%`;   
+  text.textContent = `${percent}%`;
